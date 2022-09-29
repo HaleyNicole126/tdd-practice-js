8e2f6b5ce33d23d01c410697074446087c87b73c
haybales <haleynicole126@gmail.com>
2022-09-28 22:21:44 -0600

    test driven development practice

---
+++ name-normalizer.js
@@ -0,0 +1,44 @@
+const normalize = (name) => {
+  let nameBeforeSuffix;
+  let suffix;
+  let extraneousCommaSegmenet;
+  [nameBeforeSuffix, suffix, extraneousCommaSegmenet] = name.split(', ');
+  if (extraneousCommaSegmenet) {
+    throw 'name may only contain a max of one comma';
+  }
+  if(!suffix) {
+    nameBeforeSuffix = name;
+  }
+
+    const names = nameBeforeSuffix.trim().split(' ');
+
+    if(names.length <= 1) {
+        return name;
+    }
+
+    const firstName = first(names);
+    const middleNames = names.slice(1, -1);
+    const lastName = last(names);
+    const middleInitials = middleNames.map(initialize);
+    const namesOtherThanLast = [firstName, ...middleInitials];
+
+ 
+    return `${lastName}, ${namesOtherThanLast.join(' ')}${
+      suffix ? `, ${suffix}` : ''
+    }`;
+  };
+
+  const first = array => array[0];
+  const last = array => array[array.length - 1];
+
+    const initialize = name => {
+      if (name.length > 1) {
+        return `${name.slice(0, 1)}.`
+      } else {
+        return name;
+      }
+    }
+  
+
+
+module.exports = normalize;
